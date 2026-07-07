9df582026c9e4be4dfe1def594831411c7ca20ab
thewealthyplace <thewealthyplace100@gmail.com>
2026-07-07 18:07:00 +0100

    FlipQuest.sol: add difficulty constants, Game and PlayerStats structs

--- contracts/contracts/FlipQuest.sol
+++ contracts/contracts/FlipQuest.sol
@@ -10,6 +10,27 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract FlipQuest is Ownable, ReentrancyGuard {
     IERC20 public immutable usdm;
 
+    // Difficulty 0: Classic  4x4 = 8 pairs, base 1000 pts
+    // Difficulty 1: Challenge 5x4 = 10 pairs, base 2000 pts
+    uint8[2]  public pairsCount = [8, 10];
+    uint16[2] public baseScore  = [1000, 2000];
+    uint16    public movePenalty = 20;
+
+    struct Game {
+        address player;
+        bytes32 seed;
+        uint8   difficulty;
+        uint32  startTime;
+        uint16  moves;
+        bool    completed;
+    }
+
+    struct PlayerStats {
+        uint256 bestScore;
+        uint32  gamesPlayed;
+        uint32  gamesCompleted;
+    }
+
     constructor(address _usdm) Ownable(msg.sender) {
         usdm = IERC20(_usdm);
     }
